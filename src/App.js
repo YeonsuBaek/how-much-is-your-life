ccf956d09ddfd622dfa1dc4ce6b119be1b2178cb
YeonsuBaek <soothbugger@gmail.com>
2023-02-27 20:29:01 +0900

    feat: 한 일의 최종 시간과 돈 측정하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
 import FinishedForm from './components/FinishedForm';
 import FinishedList from './components/FinishedList';
+import FinishedTotal from './components/FinishedTotal';
 
 const App = () => {
   const [finishedItems, setFinishedItems] = useState([]);
 
-  const getMoney = (start, end) => {
+  const getTime = (start, end) => {
+    let hours = Number(end.slice(0, 2)) - Number(start.slice(0, 2));
+    let minutes = Number(end.slice(3)) - Number(start.slice(3));
+
+    if (minutes >= 60) {
+      hours += Number(minutes / 60);
+      minutes %= 60;
+    }
+
+    return { hours: hours, minutes: minutes };
+  };
+
+  const getMoney = (time) => {
     const hourlyRate = 9620;
 
-    const startMinutes =
-      Number(start.slice(0, 2)) * 60 + Number(start.slice(3, 2));
-    const endMinutes = Number(end.slice(0, 2)) * 60 + Number(end.slice(3, 2));
-    const time = endMinutes - startMinutes;
-
-    return time * Number(hourlyRate / 60);
+    return (
+      (time.hours + Math.round((time.minutes / 60) * 10) / 10) * hourlyRate
+    );
   };
 
   const putItem = (item) => {
     setFinishedItems((prevItems) => {
       const currentItem = item;
-      currentItem['money'] = getMoney(item.start, item.end);
+      currentItem['time'] = getTime(item.start, item.end);
+      currentItem['money'] = getMoney(currentItem.time);
       return [currentItem, ...prevItems];
     });
   };
@@ -28,6 +39,7 @@ const App = () => {
     <div>
       <FinishedForm getItem={putItem} />
       <FinishedList finishedItems={finishedItems} />
+      <FinishedTotal finishedItems={finishedItems} />
     </div>
   );
 };
---
+++ src/components/FinishedTotal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const FinishedTotal = ({ finishedItems }) => {
+  console.log(finishedItems);
+  let totalHours = finishedItems.reduce((acc, cur) => {
+    return acc + cur.time.hours;
+  }, 0);
+  let totalMinutes = finishedItems.reduce((acc, cur) => {
+    return acc + cur.time.minutes;
+  }, 0);
+
+  if (totalMinutes >= 60) {
+    totalHours = Number(totalHours) + Number(totalMinutes / 60);
+    totalMinutes = Number(totalMinutes) % 60;
+  }
+
+  let totalMoney = finishedItems.reduce((acc, cur) => {
+    return acc + cur.money;
+  }, 0);
+
+  return (
+    <div>
+      <h2>최종 환산</h2>
+      <span>
+        총 {totalHours}시간 {totalMinutes > 0 && `${totalMinutes}분`}
+      </span>
+      <strong>총 {totalMoney}원</strong>
+    </div>
+  );
+};
+
+export default FinishedTotal;
