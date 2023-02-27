51008181aad9c0ee05bf9702071064f9c5553d72
YeonsuBaek <soothbugger@gmail.com>
2023-02-27 20:35:19 +0900

    feat: Header 구현

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>How much is your life?</h1>
+      <p>
+        당신이 생산적인 활동을 했던 시간을 돈으로 환산해줍니다. <br />
+        최저시급인 9,620원을 기준으로 계산합니다.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
