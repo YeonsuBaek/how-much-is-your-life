d066d7e0d0bec7fe370ba4b6f3daa636f3e42ca8
YeonsuBaek <soothbugger@gmail.com>
2023-02-28 21:27:34 +0900

    style: Header 컴포넌트 스타일 지정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>How much is your life?</h1>
-      <p>
+    <div className='header'>
+      <h1 className='title'>How much is your life?</h1>
+      <p className='description'>
         당신이 생산적인 활동을 했던 시간을 돈으로 환산해줍니다. <br />
         최저시급인 9,620원을 기준으로 계산합니다.
       </p>
