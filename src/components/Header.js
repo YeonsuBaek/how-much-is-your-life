0b5b79e553b013437e219aa2fde6b9fcc9722405
YeonsuBaek <soothbugger@gmail.com>
2023-02-28 21:53:04 +0900

    style: Header 내 버튼 스타일 부여

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,10 @@ const Header = ({ onOpen }) => {
         당신이 생산적인 활동을 했던 시간을 돈으로 환산해줍니다. <br />
         최저시급인 9,620원을 기준으로 계산합니다.
       </p>
-      <button onClick={() => onOpen(true)}>얼마인지 보러가기</button>
+      <button className='startButton' onClick={() => onOpen(true)}>
+        <i className='startButtonIcon'></i>
+        <span>얼마인지 보러가기</span>
+      </button>
     </div>
   );
 };
