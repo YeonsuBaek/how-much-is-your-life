112caafb363b30a48453314af89b5ba52af4660e
yeonsuBaek <soothbugger@gmail.com>
2023-03-03 15:23:43 +0900

    style: ErrorModal 컴포넌트에 스타일 부여

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './ErrorModal.css';
 
 const ErrorModal = () => {
-  return <div>이전에 입력한 시간과 겹칠 수 없습니다.</div>;
+  return (
+    <div className='errorModal'>
+      <strong className='errorMessage'>
+        이전에 입력한 시간과 겹칠 수 없습니다.
+      </strong>
+    </div>
+  );
 };
 
 export default ErrorModal;
